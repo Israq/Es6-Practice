5166514c3b5eed608ab29c8c5ed093cc8bc03d10
Syed Ragib Israq <35467653+Israq@users.noreply.github.com>
2021-02-04 10:54:38 +0600

    fisrt

---
+++ threedots.js
@@ -0,0 +1,5 @@
+const ages1 = [12,332,432,2234,,12312];
+const ages2 = [43,11,432,225,666,22];
+const ahes3 = [111,10123,1232,344,123,13434];
+const result = [...ages1,...ages2,...ahes3,54];
+console.log(result);
