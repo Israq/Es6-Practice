f5c78b54b19d9952f98fded025bbe79de30a621d
Syed Ragib Israq <35467653+Israq@users.noreply.github.com>
2021-02-04 09:54:52 +0600

    first

---
+++ default.js
@@ -0,0 +1,5 @@
+function sub(num1, num2 = 4){
+    return num1 - num2;
+}
+const total = sub(34);
+console.log(total);
---
+++ letconst.js
@@ -0,0 +1,7 @@
+let name = "israq";
+name = "syed";
+console.log(name);
+
+const name1 = "israq";
+//name1 = "syed";
+console.log(name1);
