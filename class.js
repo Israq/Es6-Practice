a6ebf70a08be24f1265277c168bd486b978d2ab0
Syed Ragib Israq <35467653+Israq@users.noreply.github.com>
2021-02-04 11:17:03 +0600

    fisrt

---
+++ class.js
@@ -0,0 +1,13 @@
+class Student{
+    constructor(sId,sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "X,Y,Z"; 
+
+    }
+    
+}
+
+const student1 = new Student(12,"Ekta");
+const student2 = new Student(44,"arekta");
+console.log(student1,student2);
